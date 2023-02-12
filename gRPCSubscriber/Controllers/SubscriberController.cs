267cc245f2570db90c0c05a65e3abb747f21e702
dogruyolomerfurkan <dogruyolomerfurkan@gmail.com>
2023-02-12 14:19:41 +0300

    init

---
+++ gRPCSubscriber/Controllers/SubscriberController.cs
@@ -0,0 +1,25 @@
+﻿using gRPCSubscriber.SyncDataServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gRPCSubscriber.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriberController : ControllerBase
+    {
+        private readonly UserDataClient _userDataClient;
+
+        public SubscriberController(UserDataClient userDataClient)
+        {
+            _userDataClient = userDataClient;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = _userDataClient.GetUserList();
+            return Ok( result);
+        }
+    }
+}
+
---
+++ gRPCSubscriber/SyncDataServices/UserDataClient.cs
@@ -0,0 +1,40 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using Mapster;
+using System.Collections.Generic;
+
+namespace gRPCSubscriber.SyncDataServices
+{
+    public class UserDataClient
+    {
+        public List<GetUserListModel> GetUserList()
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:7500");
+            var client = new UserService.UserServiceClient(channel);
+            var request = new GetUserListRequest();
+
+            try
+            {
+                var response = client.GetUserList(request);
+
+                var userList = new List<GetUserListModel>();
+                foreach (var item in response.UserList)
+                {
+                    userList.Add(new()
+                    {
+                        BirthDate = item.BirthDate,
+                        UserId = item.UserId,
+                        Name = item.Name,
+                        Surname = item.Surname
+                    });
+                }
+                return userList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw; 
+            }
+        }
+    }
+}
